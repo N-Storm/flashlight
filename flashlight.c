c56c49d8f48ac6969cc70102d6320759ded2aa8d
N-Storm <N-Storm@users.noreply.github.com>
2024-06-23 04:02:56 +0300

    New experimental variant with one more mains PWM grade

--- flashlight.c
+++ flashlight.c
@@ -14,19 +14,25 @@
 #define IN_BTN 3 // GP3
 #define IN_BTN_GP GP3
 
-//Timer0 Registers Prescaler = 64 - TMR0 Preset = 230 - Freq = 600.96 Hz - Period = 0.001664 seconds
+//Timer0 Settings: Prescaler = 4, TMR0 count = 250, Freq = 1000 Hz, Period = 1 ms
 // Soft PWM preload values for timer, affects PWM frequency
-#define TMR0_PRELOAD 230
-#define TMR0_CNT 255 - TMR0_PRELOAD
+// #define TMR0_CNT 250
+// #define TMR0_PRELOAD 256 - TMR0_CNT
 // These values are for PWM modes duty cycle. While one are used as HIGH level period other are used low for LOW mode
 // and vice-versa for othermode, resulting in 33% / 66% duty cycle for LOW / MED modes.
-#define TMR0_PWM_VAL0 (const uint8_t)(255 - ((TMR0_CNT) * 0.34))
-#define TMR0_PWM_VAL1 (const uint8_t)(255 - ((TMR0_CNT) * 0.66))
+// #define TMR0_PWM_VAL0 (const uint8_t)(256 - ((TMR0_CNT) * 0.34)) // 34% 85
+// #define TMR0_PWM_VAL1 (const uint8_t)(256 - ((TMR0_CNT) * 0.66)) // 66% 165
+// #define TMR0_PWM_VAL0 (const uint8_t)(256 - ((TMR0_CNT) * 0.3) + 2 ) // 30% 75 + adjust
+// #define TMR0_PWM_VAL1 (const uint8_t)(256 - ((TMR0_CNT) * 0.7) + 12) // 70% 175 + adjust
+#define TMR0_PWM_VAL0 25
+#define TMR0_PWM_VAL1 77
+#define TMR0_PWM_VAL2 182
+
 
 // Delays for buttons
 // Longs press equals 0.6s (cnt divided by one timer cycle period)
-#define BTN_BUF_DELAY_CNT (const uint16_t)(0.6/0.001664)
-#define BTN_BUF_DEBOUNCE_CNT 10
+#define BTN_BUF_DELAY_CNT (const uint16_t)(0.6*1000)
+#define BTN_BUF_DEBOUNCE_CNT 15
 
 #include <xc.h>
 #include <stdint.h>        /* For uint8_t definition */
@@ -36,7 +42,7 @@
 #define _XTAL_FREQ 4000000 // 4 MHz INTOSC
 
 typedef enum {bounce, short_press, long_press} buttons_t;
-typedef enum {power_on, sleep, wake, main_high, main_med, main_low, aux_white, aux_red} state_t;
+typedef enum {power_on, sleep, wake = TMR0_PWM_VAL0 - 3, aux_white = TMR0_PWM_VAL0 - 2 , aux_red = TMR0_PWM_VAL0 - 1, main_low = TMR0_PWM_VAL0, main_med = TMR0_PWM_VAL1, main_high = TMR0_PWM_VAL2, main_full = TMR0_PWM_VAL0 - 4} state_t;
 
 typedef struct {
     state_t main;
@@ -49,7 +55,7 @@ settings_t settings  __at(0x1A);
 buttons_t btn = bounce;
 
 inline void init() {
-    OPTION = ~(nGPWU | T0CS | PSA | PS1); // GPWU (wake-up on GP change) enabled, T0 = internal clock, Fosc/4, Prescaler assigned to TMR0, 1:64 prescaler
+    OPTION = ~(nGPWU | T0CS | PSA | PS2 | PS1); // GPWU (wake-up on GP change) enabled, T0 = internal clock, Fosc/4, Prescaler assigned to TMR0, 1:4 prescaler
     if (!GPWUF && (nPD || __powerdown)) { // 1st power on (i.e. not waking from sleep mode)
         GPIO = (1 << OUT_WHITE) | (1 << OUT_RED) | (1 << OUT_XML); // Start with every output at HIGH
         state = power_on;
@@ -71,17 +77,7 @@ void enter_sleep() {
 void timed_step() {
     static uint16_t btn_buf;
 
-    TMR0 = TMR0_PWM_VAL0; // preset for timer register
-    if (state == main_low)
-        GPIO = ~(1 << OUT_XML);
-    else if (state == main_med)
-        GPIO = 0xFF;
-    while (TMR0 >= TMR0_PWM_VAL0);
-
-    TMR0 = TMR0_PWM_VAL1;
-    if (state == main_low)
-        GPIO = 0xFF;
-    else if (state == main_med)
+    if (state >= main_low)
         GPIO = ~(1 << OUT_XML);
 
     if (!IN_BTN_GP) {
@@ -96,8 +92,13 @@ void timed_step() {
             btn = short_press;
         btn_buf = 0; // reset counter when button are not pressed
     }
-
-    while (TMR0 >= TMR0_PWM_VAL1);
+    
+    while (TMR0 < state);
+    if (state >= main_low) {
+        GPIO = 0xFF;
+        while (TMR0 >= state);
+    } else
+       while (TMR0 > 0);
 }
 
 void fsm_routine() {
@@ -133,6 +134,14 @@ void fsm_routine() {
             }
             break;
         case main_high:
+            if (btn == short_press)
+                state = sleep;
+            else if (btn == long_press) {
+                state = main_full;
+                settings.main = state;
+            }
+            break;
+        case main_full:
             GPIO = ~(1 << OUT_XML);
             if (btn == short_press)
                 state = sleep;
